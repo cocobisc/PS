e81344e83c8ab2cf1b0a90b6368f291feb22ad8f
cocobisc <xmfhs99@gmail.com>
2020-08-06 00:05:57 +0900

    다이어트

---
+++ 백준/1484- 다이어트.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int g; cin>>g;
+    bool flag = 0;
+    long long s,e;
+    s = e = 1;
+
+    while (1) {
+        if (e - s == 1 && e*e - s*s > g) break;
+        if (e*e - s*s <= g) {
+            if (e*e - s*s == g) {
+                cout << e << '\n';
+                flag = 1;
+            }
+            e++;
+        }
+        else s++;
+    }
+    if (!flag) cout << -1;
+}
