54bfd4b39e5a307e551d1df196431d59d9a4c9fb
cocobisc <xmfhs99@gmail.com>
2020-08-05 19:39:10 +0900

    수들의 합

--- 백준/1821- 수들의 합.cpp
+++ 백준/1821- 수들의 합.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
+using namespace std;
+int arr[11][11], n, f;
 
 int main() {
-       
+	cin >> n >> f;
+	if (n == 1) cout << f;
+	for (int i = 1; i <= n; i++) arr[1][i] = i;
+
+	do {
+		for (int i = 1; i < n - 1; i++) {
+			for (int j = 1; j <= n - i; j++) 
+				arr[i + 1][j] = arr[i][j] + arr[i][j + 1];
+		}
+		if (arr[n - 1][1] + arr[n - 1][2] == f) {
+			for (int i = 1; i <= n; i++) cout << arr[1][i] << " ";
+			break;
+		}
+	} while (next_permutation(arr[1] + 1, arr[1] + n + 1));
 }
