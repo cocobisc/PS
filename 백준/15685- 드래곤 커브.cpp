0a3c2fea4d877d39f5dd536a14274c037282222d
cocobisc <xmfhs99@gmail.com>
2020-09-15 17:15:16 +0900

    Add files

---
+++ 백준/15685- 드래곤 커브.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[] = {1, 0, -1, 0};  //¿À À§ ¿Þ ¾Æ
+int dy[] = {0, -1, 0, 1};
+int check[101][101];
+int ans, n;
+
+void getcnt() {
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (check[i][j] && check[i + 1][j] && check[i][j + 1] && check[i + 1][j + 1])
+                ans++;
+        }
+    }
+}
+
+void make_curve(int x, int y, int g, int cnt, vector<int> v) {
+    if (cnt == g + 1) return;
+    vector<int> temp = v;
+
+    for (int i = v.size() - 1; i >= 0; i--) {
+        int ndir = (v[i] + 1) % 4;
+        x = x + dx[ndir], y = y + dy[ndir];
+        check[y][x] = 1;
+        temp.push_back(ndir);
+    }
+    make_curve(x, y, g, cnt + 1, temp);
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x, y, d, g;
+        cin >> x >> y >> d >> g;
+		int nx = x + dx[d], ny = y + dy[d];
+        check[y][x] = 1;
+        check[ny][nx] = 1;
+        make_curve(nx, ny, g, 1, {d});
+    }
+    getcnt();
+
+    cout << ans;
+    return 0;
+}
---
+++ 백준/7432- 디스크 트리.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node {
+	map<string, node*> next;
+} Node;
+
+vector<string> path;
+
+void make_tree(Node *node, string s, int idx) {
+	string name;
+	int i = 0;
+	for (; i < s.size(); i++) {
+		if (s[i] == '\\') break;
+		name += s[i];
+	}
+	if (i < s.size() - 1 && node->next[name]) {
+		make_tree(node->next[name], s.substr(i + 1), idx + 1);
+		return;
+	}
+	for (int i = 0; i < idx; i++) cout << " ";
+	cout << name << '\n';
+	node->next[name] = new Node;
+
+	if (i < s.size() - 1)
+		make_tree(node->next[name], s.substr(i + 1), idx + 1);
+
+}
+
+bool cmp(string &a, string &b) {
+	int mi = min(a.size(), b.size());
+	for (int i = 0; i < mi; i++) {
+		if (a[i] != b[i]) {
+			if (a[i] == '\\')
+				return true;
+			else if (b[i] == '\\')
+				return false;
+			return a[i] < b[i];
+		}
+	}
+	return a < b;
+}
+
+int main() {
+	int n;
+	cin >> n;#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node {
+	map<string, node*> next;
+} Node;
+
+vector<string> path;
+
+void make_tree(Node *node, string s, int idx) {
+	string name;
+	int i = 0;
+	for (; i < s.size(); i++) {
+		if (s[i] == '\\') break;
+		name += s[i];
+	}
+	if (i < s.size() - 1 && node->next[name]) {
+		make_tree(node->next[name], s.substr(i + 1), idx + 1);
+		return;
+	}
+	for (int i = 0; i < idx; i++) cout << " ";
+	cout << name << '\n';
+	node->next[name] = new Node;
+
+	if (i < s.size() - 1)
+		make_tree(node->next[name], s.substr(i + 1), idx + 1);
+
+}
+
+bool cmp(string &a, string &b) {
+	int mi = min(a.size(), b.size());
+	for (int i = 0; i < mi; i++) {
+		if (a[i] != b[i]) {
+			if (a[i] == '\\')
+				return true;
+			else if (b[i] == '\\')
+				return false;
+			return a[i] < b[i];
+		}
+	}
+	return a < b;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	path.resize(n);
+	for (int i = 0; i < n; i++)
+		cin >> path[i];
+	sort(path.begin(), path.end(), cmp);
+
+	Node root;
+	for (string s : path)
+		make_tree(&root, s, 0);
+}
+	path.resize(n);
+	for (int i = 0; i < n; i++)
+		cin >> path[i];
+	sort(path.begin(), path.end(), cmp);
+
+	Node root;
+	for (string s : path)
+		make_tree(&root, s, 0);
+}
